757d5731205cbfc1b11ed496bee20d6e63fa33ea
Kyle Conroy <kyle@kyleconroy.com>
2016-10-22 20:11:31 -0700

    Add the greeter experiment

---
+++ python/helloworld/greeter.py
@@ -0,0 +1,18 @@
+import asyncio
+import helloworld
+
+class Greeter(helloworld.Greeter):
+    async def say_hello(self, ctx, msg):
+        return helloworld.HelloReply(message='Hello, %s!' % msg.name)
+
+
+async def run():
+    client = helloworld.GreeterClient(Greeter())
+    resp = await client.say_hello(name='you')
+    print("Greeter client received: " + resp.message)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()  
+    loop.run_until_complete(run())  
+    loop.close()
